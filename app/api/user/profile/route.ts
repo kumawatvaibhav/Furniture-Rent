3bc02fe247e29055c0b8a5b8833f77130c6dcbc0
vaibhav <vaibhavkumawat21318@gmail.com>
2024-10-25 10:28:38 +0530

    build error resolve

--- app/api/user/profile/route.ts
+++ app/api/user/profile/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
   try {
-    return NextResponse.json("hello loading", { status: 200 });
+    return NextResponse.json("hello loading is very good", { status: 200 });
 
   } catch (err) {
     console.error('Error fetching profile data:', err);
